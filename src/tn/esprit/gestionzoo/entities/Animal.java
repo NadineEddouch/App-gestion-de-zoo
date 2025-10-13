f3f32c49280aae53cbc8f4135f969bec82221ef0
NadineEddouch <Nadine.Eddouch@esprit.tn>
2025-10-13 18:11:23 +0100

    Update fonctions prosit5

--- src/tn/esprit/gestionzoo/entities/Animal.java
+++ src/tn/esprit/gestionzoo/entities/Animal.java
@@ -1,11 +1,11 @@
 package tn.esprit.gestionzoo.entities;
-public class Animal {
+public sealed class Animal permits Aquatic, Terrestrial {
     private String family;
     private String name;
-    private int age;
-    private boolean isMammal;
-
+    private  int age;
+    public boolean isMammal;
     public Animal(){}
+
     // Constructeur paramétré
     public Animal(String family, String name, int age, boolean isMammal) {
         this.family = family;
@@ -13,39 +13,40 @@ public class Animal {
         this.age = age;
         this.isMammal = isMammal;
     }
+    @Override
+    public String toString() {
+        return "tn.esprit.gestionzoo.entities.Animal [Famille=" + family + ", Nom=" + name + ", Âge=" + age + ", Mammifère=" + isMammal ;
+    }
+
     public String getFamily() {
         return family;
     }
+
     public void setFamily(String family) {
         this.family = family;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
-        this.name = name;
+        if(name != null ) {
+            this.name = name;
+        } else {
+            System.out.println("Erreur : le nom de l'animal ne peut pas être vide.");
+        }
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
-        if (age < 0) {
-            System.out.println("L'age de l'animal doit être positif.");
-        }
-        else {
+        if(age >= 0) {
             this.age = age;
+        } else {
+            System.out.println("Erreur : l'âge de l'animal ne peut pas être négatif.");
         }
     }
-    public boolean isIsMammal() {
-        return isMammal;
-    }
-    public void setIsMammal(boolean isMammal) {
-        this.isMammal = isMammal;
-    }
-
-    @Override
-    public String toString() {
-        return "Animal [Famille=" + family + ", Nom=" + name + ", Âge=" + age + ", Mammifère=" + isMammal + "]";
-    }
-
 }
--- src/tn/esprit/gestionzoo/main/ZooManagement.java
+++ src/tn/esprit/gestionzoo/main/ZooManagement.java
@@ -1,9 +1,8 @@
 package tn.esprit.gestionzoo.main;
 
-import java.util.Scanner;
-import tn.esprit.gestionzoo.entities.Animal;
-import tn.esprit.gestionzoo.entities.Zoo;
+import tn.esprit.gestionzoo.entities.*;
 
+import java.util.Scanner;
 
 public class ZooManagement {
     public static void main(String[] args) {
@@ -30,63 +29,83 @@ public class ZooManagement {
         lion.setFamily("Félidé");
         lion.setName("Lion");
         lion.setAge(5);
-        lion.setIsMammal(true);
+        lion.isMammal = true;
 
         Zoo myZoo = new Zoo();
         myZoo.setName("Parc Animalier");
-        myZoo.setCity("Tunis");
+        myZoo.city = "Tunis";
+        // myZoo.nbrCages = 20;
 
-        System.out.println("Zoo : " + myZoo.getName() + " à " + myZoo.getCity());
-        System.out.println("Animal : " + lion.getName() + " (" + lion.getFamily() + ")");
+        // System.out.println("tn.esprit.gestionzoo.entities.Zoo : " + myZoo.name + " à " + myZoo.city);
+        System.out.println("tn.esprit.gestionzoo.entities.Animal : " + lion.getName() + " (" + lion.getFamily() + ")");
 
         Animal lion1 = new Animal("Félidé", "Lion", 5, true);
-        Zoo myZoo1 = new Zoo("Parc Animalier", "Tunis");
+        // tn.esprit.gestionzoo.entities.Zoo myZoo1 = new tn.esprit.gestionzoo.entities.Zoo("Parc Animalier", "Tunis", 20);
 
-        System.out.println("Animal créé : " + lion1.getName());
-        System.out.println("Zoo créé : " + myZoo1.getName() + " (" + myZoo.getCity() + ")");
+        System.out.println("tn.esprit.gestionzoo.entities.Animal créé : " + lion1.getName());
+        //System.out.println("tn.esprit.gestionzoo.entities.Zoo créé : " + myZoo1.name + " (" + myZoo.city + ")");
 
         myZoo.displayZoo();
         System.out.println(myZoo);
+        //Prosit 3
+        Zoo zoo1 = new Zoo("tn.esprit.gestionzoo.entities.Zoo Esprit", "Tunis");
+        Zoo zoo2 = new Zoo("tn.esprit.gestionzoo.entities.Zoo Safari", "Sousse");
 
-        // Prosit 3
-        Animal tiger = new Animal("Félidé" , "Tigre" ,4 , true);
-        Animal Crocodile = new Animal("Crocodylidae" , "Crocodile", 8, false);
+        Animal tigre = new Animal("Félidé", "Shere Khan", 7, true);
+        Animal lion2 = new Animal("Félidé", "Lion", 5, true); // identique à lion
+        // Test Ajout
+        zoo1.addAnimal(lion);
+        zoo1.addAnimal(tigre);
+        zoo1.addAnimal(lion2); // doit être refusé (doublon)
 
-        myZoo.addAnimal(lion);
-        myZoo.addAnimal(tiger);
-        myZoo.addAnimal(Crocodile);
+        // Test Affichage
+        zoo1.displayAnimals();
 
-        myZoo.showAnimals();
+        // Test Recherche
+        System.out.println("Indice : " + zoo1.searchAnimal(lion));
+        System.out.println("Indice : " + zoo1.searchAnimal(tigre));
 
-        myZoo.searchAnimal(tiger);
-        myZoo.searchAnimal(lion);
-        myZoo.searchAnimal(Crocodile);
+        // Test Suppression
+        zoo1.removeAnimal(tigre);
+        zoo1.displayAnimals();
 
-        Animal Giraffe = new Animal("Giraffidae", "Giraffe",10,true);
-        myZoo.searchAnimal(Giraffe);
+        // Test tn.esprit.gestionzoo.entities.Zoo plein
+        for (int i = 0; i < 30; i++) {
+            zoo1.addAnimal(new Animal("TestFamily", "tn.esprit.gestionzoo.entities.Animal" + i, 2, false));
+        }
+        System.out.println("tn.esprit.gestionzoo.entities.Zoo plein " + zoo1.isZooFull());
 
-        Animal lion2 = new Animal("Félidé", "Lion", 5, true);
-        myZoo.searchAnimal(lion2);
+        // Test Comparaison
+        zoo2.addAnimal(new Animal("Canidés", "Wolf", 4, true));
+        Zoo plusGrand = Zoo.comparerZoo(zoo1, zoo2);
+        System.out.println("tn.esprit.gestionzoo.entities.Zoo avec le plus d'animaux : " + plusGrand);
 
-        myZoo.addAnimal(tiger);
-        myZoo.addAnimal(Giraffe);
+        //Prosit 5
+        Aquatic aquatic = new Aquatic();
+        Terrestrial terrestrial = new Terrestrial();
+        Dolphin dolphin = new Dolphin();
+        Penguin penguin = new Penguin();
 
-        myZoo.removeAnimal(tiger);
-        myZoo.showAnimals();
+        System.out.println(aquatic);
+        System.out.println(terrestrial);
+        System.out.println(dolphin);
+        System.out.println(penguin);
 
-        myZoo.isZooFull();
+        Aquatic aquatic2 = new Aquatic("Poisson", "Poisson-clown", 2, false, "océan");
+        Terrestrial terrestrial2 = new Terrestrial("Canidé", "Chien", 3, true, 4);
+        Dolphin dolphin2 = new Dolphin("Cétacé", "Flipper", 6, true, "mer", 25.5f);
+        Penguin penguin2 = new Penguin("Sphéniscidé", "Pingou", 4, false, "bassin", 15.8f);
 
-        Zoo zoo2 = new Zoo("Safari Paradis", "Sousse" );
-        zoo2.addAnimal(tiger);
-        zoo2.addAnimal(Giraffe);
+        System.out.println(aquatic2);
+        System.out.println(terrestrial2);
+        System.out.println(dolphin2);
+        System.out.println(penguin2);
 
-        Zoo zoo3 = new Zoo("Zoo Aventure", "Tunis");
-        zoo3.addAnimal(tiger);
-        zoo3.addAnimal(Crocodile);
-        zoo3.addAnimal(Giraffe);
+        aquatic2.swim();
+        dolphin2.swim();
+        penguin2.swim();
 
-        Zoo zooPlusAnim = Zoo.comparerZoo(zoo2,zoo3);
-        System.out.println("Le zoo avec le plus d'animaux est  : " + zooPlusAnim.getName());
+
 
     }
 }
